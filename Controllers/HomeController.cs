cd8e4bf2b57a415b0c9d763caa835281ca68b90d
Kenneth <kenneth_motta@usmp.pe>
2021-07-17 19:23:23 -0500

    v170721beta

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using seeker.Models;
+using seeker.Data;
 
 namespace seeker.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DatabaseContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+         DatabaseContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -23,9 +27,39 @@ namespace seeker.Controllers
             return View();
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Listar()
+        {
+            return View();
+        }
+
+        public IActionResult CCreate(Create objcreate)
+        {
+             if (ModelState.IsValid)
+            {
+                 
+                 objcreate.Mensaje = "El Usuario a sido Registrado con Éxito"; 
+
+                 _context.Add(objcreate);
+                 _context.SaveChanges();
+
+                 return View("Create",objcreate);   
+            }
+            return View("Create");
+        }
+
+        public IActionResult GetCreate()
+        {
+             var listCreate=_context.Creates.ToList();
+             return View("Listar", listCreate);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Models/Create.cs
+++ Models/Create.cs
@@ -3,27 +3,31 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace seeker.Models{
-    [Table("t_loan")]
+    [Table("t_users")]
     public class Create {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("id")]
         public int ID {get; set;}
-        [Required(ErrorMessage = "Please enter name")]
-        [Column("name")]
-        public string name {get;set;}
+        [Required(ErrorMessage = "Please enter first name")]
+        [Column("first_name")]
+        public string first_name {get;set;}
+
+        [Required(ErrorMessage = "Please enter last name")]
+        [Column("last_name")]
+        public string last_name {get;set;}
 
          [Required(ErrorMessage = "Please enter date")]
         [Column("fecha")]
         [DataType(DataType.Date)]
-        public DateTime loan_date {get; set;}
+        public DateTime fecha {get; set;}
 
         [Required(ErrorMessage = "Please enter book")]
         [Column("libro")]
-        public double original_amount {get; set;}
+        public double libro {get; set;}
 
         [Required(ErrorMessage = "Please enter page")]
         [Column("pagina")]
-        public double rate {get; set;}
+        public double pagina {get; set;}
 
         [NotMapped]
         public string Mensaje { get; set; }
